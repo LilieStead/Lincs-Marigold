55c350a677884b1172f1b396423f025bb5f25511
Lilie Stead <liliestead@hotmail.com>
2023-04-01 23:23:41 +0100

    Contact form browser update 1
    styled from
    added value counter (js)
    added textarea to form instead of text input

--- contact.php
+++ contact.php
@@ -12,18 +12,32 @@ include "assets/inc/header.php";
         <div id="contactdiv">
             <h1 class="title">Contact us here!</h1>
             <form id="contactform" action="assets/proc/sendformprocess.php" method="post">
-                <label for="title">Title</label>
-                <select name="title" id="title">
-                    <option value="Mr">Mr</option>
-                    <option value="Miss">Miss</option>
-                    <option value="Mrs">Mrs</option>
-                </select>
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name">
-                <label for="email">Email</label>
-                <input type="text" id="email" name="email">
-                <label for="query">Query</label>
-                <input type="text" id="query" name="query">
+                <div class="flexdiv">
+                    <div id="titlediv">
+                        <label for="title">Title</label>
+                        <select name="title" id="title" class="halfspace">
+                            <option value="Mr">Mr</option>
+                            <option value="Miss">Miss</option>
+                            <option value="Mrs">Mrs</option>
+                        </select>
+                    </div>
+                    <div id="namediv">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" class="halfspace">
+                    </div>
+                </div>
+                <div id="emaildiv">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email">
+                </div>
+                <div class="querydiv">
+                    <label for="query">Query</label>
+                    <!-- Each key press will get value of each character in the text area  -->
+                    <textarea name="query" id="query" cols="70" rows="10" 
+                     maxlength="255" onkeyup="charcountupdate(this.value)"></textarea> 
+                </div>
+                <!-- Outputs used characters  -->
+                <span id=counter></span>
                 <input type="submit">
             </form>
             <p class="smalltxt"><i class="fa fa-phone" aria-hidden="true"></i> alliteratively call: 07526292791 </p>
@@ -34,3 +48,6 @@ include "assets/inc/header.php";
 <?php
 include "assets/inc/footer.php";
 ?>
+
+<!-- Finds java script to count charters in text area-->
+<script src="assets/js/wordcounter255.js"></script>
