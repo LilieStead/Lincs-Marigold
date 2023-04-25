46954cf8e213d5f8f385a384a4ae3ade5277fa0b
Lilie Stead <liliestead@hotmail.com>
2023-04-25 19:23:59 +0100

    Cookies Overlay
    Cookies Overlay attempted

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -12,6 +12,14 @@
     <button class="overlaybutton" onclick="hideoverlay()">Hide Overlay</button>
   </div>
 
+<<<<<<< Updated upstream
+=======
+<!-- Aa div used to store buttons -->
+<div id="overlaysettings">
+  <!-- buttons used to open and close overlay -->
+  <button class="overlaybutton" onclick="showoverlay(); document.cookie = 'show_overlay=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'">Open Overlay</button>
+  <button class="overlaybutton" onclick="hideoverlay()">Hide Overlay</button>
+>>>>>>> Stashed changes
 </div>
 
 </footer>
@@ -28,5 +36,12 @@
   }
 //   Java script used to get the year 
     document.getElementById("copyrightyear").innerHTML = new Date().getFullYear();
+  
+
+    if (document.cookie.indexOf("show_overlay=true") !== -1) {
+    // display the overlay
+    showoverlay();
+  }
+  
 </script>
 
