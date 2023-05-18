bf3207e6a53433248bff2a19af368dd44f6313fd
Lilie Stead <liliestead@hotmail.com>
2023-05-18 22:30:10 +0100

    Code comments

--- assets/inc/databaseconnection.php
+++ assets/inc/databaseconnection.php
@@ -1,3 +1,3 @@
 <?php
-
+// used to connect to the database in XAMPP
 $connection = mysqli_connect("localhost","root", "", "lincsmarigold"); 
--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -19,51 +19,62 @@
 
 
 <script>
+  // overlay toggle on and off  
   let overlayActive = false;
 
+  
+
   function toggleoverlay() {
-    
+    // if not false change to true on click 
     if(!overlayActive)
     {
       
       overlayActive = true;
+      // makes element visible 
       document.getElementById("overlay").style.display = "block";
 
     }
     else{
       overlayActive = false;
+      // makes element hidden
       document.getElementById("overlay").style.display = "none";
 
     }
   }
+  // END OF OVERLAY TOGGLE 
+
+
 // used to change the colour of the overlay
   function changecolour(){
     var colourinput = document.getElementById("colourchange");
+    // sotres uses value
     var colour = colourinput.value;
+    // aplays the value to the overlay
     document.getElementById("overlay").style.backgroundColor=colour;
   }
-//   Java script used to get the year 
+  // END OF OVERLAY COLOUR CHANGE 
+
+
+  // Java script used to get the year 
     document.getElementById("copyrightyear").innerHTML = new Date().getFullYear();
-  
-    let originalFontSize;
-let textsize = false;
+  // END OF GET YEAR 
 
+    // used to store the original size of font 
+  let originalFontSize;
+  // used to determent if text size is on or off 
+  let textsize = false;
 
-
-
-
-
-
+// used to toggle font size
 function toggletext() {
   const paragraphs = document.querySelectorAll("p");
   var footer = document.querySelector("#footerid");
 
-  
+  // if text is false chnage to true 
   if (!textsize) {
     textsize = true;
     footer.classList.add("footersize");
     
-    // Store the original font size if not already stored
+    // changes back to original font size 
     if (!originalFontSize) {
       
       originalFontSize = [];
@@ -71,18 +82,21 @@ function toggletext() {
         originalFontSize.push(p.style.fontSize);
       });
     }
-    
+    // for each p tag change the font size 
     paragraphs.forEach(p => {
       p.style.fontSize = "1.3em"; // update font size to 20px
     });
   } else {
+    // remove the class from the footer as font is back to its original size
     footer.classList.remove("footersize");
     textsize = false;
+    // change p tags to original size 
     paragraphs.forEach((p, index) => {
       p.style.fontSize = originalFontSize[index]; // revert to original font size
     });
   }
 }
+// END OF FONT CHANGE 
 
 
 </script>
